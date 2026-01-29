96ebf0027dadcf7778bf6039b7b845c2e995a6a8
김예안 <kjan0929@naver.com>
2026-01-29 16:06:18 +0900

    feat(render): 투두 렌더링 로직 구현

--- todo-list/app.js
+++ todo-list/app.js
@@ -9,3 +9,32 @@ const options = {
 
 const formattedDate = today.toLocaleDateString('en-US', options);
 document.querySelector('.date-wrapper__full-date').textContent = formattedDate;
+
+// 투두 렌더링
+let todos = [
+  { id: 1, content: '코멘토 2주차 과제 수행', done: false },
+  { id: 2, content: '프로젝트 코드 리뷰', done: true },
+];
+
+const todoListEl = document.querySelector('.todo-list');
+
+const render = () => {
+  const htmlString = todos.map(
+    (todo) => `
+        <li class="todo ${todo.done ? 'done' : ''}" data-id="${todo.id}">
+            <button type="button" class="icon-button todo__status-button">
+              <img src="./assets/${
+                todo.done ? 'done.svg' : 'todo.svg'
+              }" alt="todo-status-button" />
+            </button>
+            <p class="todo__content">${todo.content}</p>
+            <button type="button" class="icon-button todo__delete-button">
+              <img src="./assets/delete.svg" alt="todo-delete-button" />
+            </button>
+        </li>
+        `
+  );
+
+  todoListEl.innerHTML = htmlString.join('');
+};
+render();
